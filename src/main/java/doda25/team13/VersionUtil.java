d66cfb005fa78036b991f4fa0376a3b5735f95e2
PG-20 <pranav20111998@gmail.com>
2025-11-19 17:47:21 +0100

    Add main to check if it's running fine, add .gitignore

--- src/main/java/doda25/team13/VersionUtil.java
+++ src/main/java/doda25/team13/VersionUtil.java
@@ -6,4 +6,12 @@ public class VersionUtil {
         String version = VersionUtil.class.getPackage().getImplementationVersion();
         return version != null ? version : "unknown";
     }
+
+    public static void main(String[] args) {
+        System.out.println("--- Library Version Check ---");
+        System.out.println("Class Package: " + VersionUtil.class.getPackage().getName());
+        System.out.println("Implementation Title: " + VersionUtil.class.getPackage().getImplementationTitle());
+        System.out.println("Retrieved Version: " + getVersion());
+        System.out.println("-----------------------------");
+    }
 }
