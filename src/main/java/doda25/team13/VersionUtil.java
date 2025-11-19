a89e747d05dbcd3aa5c333a9a5303af4da028573
PG-20 <pranav20111998@gmail.com>
2025-11-19 18:12:16 +0100

    Add main to test

--- src/main/java/doda25/team13/VersionUtil.java
+++ src/main/java/doda25/team13/VersionUtil.java
@@ -1,9 +1,16 @@
 package doda25.team13;
-//test release
 public class VersionUtil {
 
     public static String getVersion() {
         String version = VersionUtil.class.getPackage().getImplementationVersion();
         return version != null ? version : "unknown";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--- Library Version Check ---");
+        System.out.println("Class Package: " + VersionUtil.class.getPackage().getName());
+        System.out.println("Implementation Title: " + VersionUtil.class.getPackage().getImplementationTitle());
+        System.out.println("Retrieved Version: " + getVersion());
+        System.out.println("-----------------------------");
     }
 }
